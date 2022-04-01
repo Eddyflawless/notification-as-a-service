72fe503ce871f01738946473879873dff7424b0b
Edwin Djonorh <edjonorh@gmail.com>
2022-04-01 09:00:34 +0000

    included an expected notification options object to be passed into the firebase messaging construct

--- src/utils/pusher.js
+++ src/utils/pusher.js
@@ -1,7 +1,13 @@
 import { admin } from '../firebase-config'
 
+const notification_options = {
+    priority: "high",
+    timeToLive: 60 * 60 * 24
+};
+
 exports.sendNotification = async (registrationToken, message, options={}) => {
 
+    var options = { ...notification_options, ...options };
 
     var response = await admin.messaging().sendToDevice(registrationToken, message, options);
 
